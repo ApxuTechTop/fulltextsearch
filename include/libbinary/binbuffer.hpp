d748d9a3a5878a973f48438d4136bfefbf73ba03
ApxuTechTop <apxutech@gmail.com>
2023-05-06 21:49:48 +0700

    Format files

--- include/libbinary/binbuffer.hpp
+++ include/libbinary/binbuffer.hpp
@@ -1,114 +1,128 @@
 #pragma once
 
 #include <cstddef>
-#include <vector>
+#include <libtrie/trie.hpp>
 #include <string>
 #include <unordered_map>
-#include <libtrie/trie.hpp>
-namespace binary
-{
-    class BinaryBuffer
-    {
-    private:
-        std::vector<char> buffer;
+#include <vector>
+namespace binary {
+class BinaryBuffer {
+  private:
+	std::vector<char> buffer;
 
-    public:
-        std::vector<char>& get() {
-            return buffer;
-        }
-        std::size_t write_to(const void *data, std::size_t size, std::size_t offset) {
-            for (std::size_t i = 0; i < size; ++i) {
-                char c = *(reinterpret_cast<const char*>(data) + i);
-                buffer.insert(std::next(buffer.begin(), offset + i), c); // ?
-            }
-            return offset;
-        }
-        void write(std::nullptr_t) {}
-        std::pair<std::size_t, std::size_t> write(const void *data, std::size_t size) {
-            return {write_to(data, size, buffer.size()), buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const std::string& str) {
-            return {write(static_cast<const void*>(str.data()), str.size()).first, buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const std::size_t value) {
-            return {write(static_cast<const void*>(&value), sizeof(value)).first, buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const char value) {
-            return {write(static_cast<const void*>(&value), sizeof(value)).first, buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const bool value) { // удостовериться
-            return {write(static_cast<const void*>(&value), sizeof(value)).first, buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const BinaryBuffer& bbuf) {
-            std::size_t offset = buffer.size();
-            buffer.insert(buffer.end(), bbuf.buffer.begin(), bbuf.buffer.end());
-            return {offset, buffer.size()};
-        }
-        template <class T>
-        std::pair<std::size_t, std::vector<std::size_t>> write(std::vector<T> container) {
-            std::vector<std::size_t> offsets;
-            auto offset = std::get<0>(write(container.size()));
-            for (std::size_t i = 0; i < container.size(); ++i) {
-                offsets[i] = std::get<0>(write(container[i]));
-            }
-            return {offset, offsets};
-        }
-        template <class T, class U>
-        std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t> write(std::unordered_map<T, U> container) {
-            std::unordered_map<T, std::size_t> offsets;
-            auto offset = std::get<0>(write(container.size()));
-            for (const auto& [key, value] : container) {
-                offsets[key] = std::get<0>(write(value));
-            }
-            return {offset, offsets, buffer.size()};
-        }
+  public:
+	std::vector<char> &get() { return buffer; }
+	std::size_t write_to(const void *data, std::size_t size,
+						 std::size_t offset) {
+		for (std::size_t i = 0; i < size; ++i) {
+			char c = *(reinterpret_cast<const char *>(data) + i);
+			buffer.insert(std::next(buffer.begin(), offset + i), c); // ?
+		}
+		return offset;
+	}
+	void write(std::nullptr_t) {}
+	std::pair<std::size_t, std::size_t> write(const void *data,
+											  std::size_t size) {
+		return { write_to(data, size, buffer.size()), buffer.size() };
+	}
+	std::pair<std::size_t, std::size_t> write(const std::string &str) {
+		return {
+			write(static_cast<const void *>(str.data()), str.size()).first,
+			buffer.size()
+		};
+	}
+	std::pair<std::size_t, std::size_t> write(const std::size_t value) {
+		return { write(static_cast<const void *>(&value), sizeof(value)).first,
+				 buffer.size() };
+	}
+	std::pair<std::size_t, std::size_t> write(const char value) {
+		return { write(static_cast<const void *>(&value), sizeof(value)).first,
+				 buffer.size() };
+	}
+	std::pair<std::size_t, std::size_t>
+	write(const bool value) { // удостовериться
+		return { write(static_cast<const void *>(&value), sizeof(value)).first,
+				 buffer.size() };
+	}
+	std::pair<std::size_t, std::size_t> write(const BinaryBuffer &bbuf) {
+		std::size_t offset = buffer.size();
+		buffer.insert(buffer.end(), bbuf.buffer.begin(), bbuf.buffer.end());
+		return { offset, buffer.size() };
+	}
+	template <class T>
+	std::pair<std::size_t, std::vector<std::size_t> >
+	write(std::vector<T> container) {
+		std::vector<std::size_t> offsets;
+		auto offset = std::get<0>(write(container.size()));
+		for (std::size_t i = 0; i < container.size(); ++i) {
+			offsets[i] = std::get<0>(write(container[i]));
+		}
+		return { offset, offsets };
+	}
+	template <class T, class U>
+	std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t>
+	write(std::unordered_map<T, U> container) {
+		std::unordered_map<T, std::size_t> offsets;
+		auto offset = std::get<0>(write(container.size()));
+		for (const auto &[key, value] : container) {
+			offsets[key] = std::get<0>(write(value));
+		}
+		return { offset, offsets, buffer.size() };
+	}
 
-        template <class T, class I, class V>
-        std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t> write(std::unordered_map<T, std::unordered_map<I, V>> container, std::unordered_map<I, std::size_t> other_offsets) {
-            std::size_t offset;
-            std::unordered_map<T, std::size_t> offsets;
-            offset = write(container.size()).first;
-            for (const auto& [key, value] : container) {
-                offsets[key] = std::get<0>(write(value, other_offsets));
-            }
-            return {offset, offsets, buffer.size()};
-        }
-        template <class T, class U, class V>
-        std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t> write(std::unordered_map<T, U> container, V other_offsets) {
-            std::size_t offset;
-            std::unordered_map<T, std::size_t> offsets;
-            offset = write(container.size()).first;
-            for (const auto& [key, value] : container) {
-                write(other_offsets[key]);
-                offsets[key] = std::get<0>(write(value));
-            }
-            return {offset, offsets, buffer.size()};
-        }
-        std::pair<std::size_t, std::size_t> write(const tech::Trie<std::size_t>& trie) {
-            std::unordered_map<tech::Trie<std::size_t>::Node*, std::size_t> child_offsets;
-            std::size_t offset_start = buffer.size();
-            auto nodes = trie.nodes();
-            for (auto node : nodes) {
-                auto [my_offset, my_offset_end] = write(node.has()); // записываю себя
-                if (child_offsets.count(&node) > 0) { // проверяю: а не записывал ли кто нибудь мою позицию
-                    write_to(&my_offset, sizeof(my_offset), child_offsets[&node]); // если да то надо подправить
-                }
-                for (auto* child : node) {
-                    write(child->letter);
-                }
-                for (auto* child : node) {
-                    auto [child_offset, child_offset_end] = write(my_offset); // my_offset просто ради std::size_t
-                    child_offsets[child] = child_offset;
-                }
-                write(node.is_leaf);
-                if (node.is_leaf) {
-                    write(node.value);
-                }
-            }
-            return {offset_start, buffer.size()};
-        }     
-    };
-    class BinaryReader
-    {
-    };
+	template <class T, class I, class V>
+	std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t>
+	write(std::unordered_map<T, std::unordered_map<I, V> > container,
+		  std::unordered_map<I, std::size_t> other_offsets) {
+		std::size_t offset;
+		std::unordered_map<T, std::size_t> offsets;
+		offset = write(container.size()).first;
+		for (const auto &[key, value] : container) {
+			offsets[key] = std::get<0>(write(value, other_offsets));
+		}
+		return { offset, offsets, buffer.size() };
+	}
+	template <class T, class U, class V>
+	std::tuple<std::size_t, std::unordered_map<T, std::size_t>, std::size_t>
+	write(std::unordered_map<T, U> container, V other_offsets) {
+		std::size_t offset;
+		std::unordered_map<T, std::size_t> offsets;
+		offset = write(container.size()).first;
+		for (const auto &[key, value] : container) {
+			write(other_offsets[key]);
+			offsets[key] = std::get<0>(write(value));
+		}
+		return { offset, offsets, buffer.size() };
+	}
+	std::pair<std::size_t, std::size_t>
+	write(const tech::Trie<std::size_t> &trie) {
+		std::unordered_map<tech::Trie<std::size_t>::Node *, std::size_t>
+			child_offsets;
+		std::size_t offset_start = buffer.size();
+		auto nodes = trie.nodes();
+		for (auto node : nodes) {
+			auto [my_offset, my_offset_end]
+				= write(node.has()); // записываю себя
+			if (child_offsets.count(&node)
+				> 0) { // проверяю: а не записывал ли кто нибудь мою позицию
+				write_to(&my_offset, sizeof(my_offset),
+						 child_offsets[&node]); // если да то надо подправить
+			}
+			for (auto *child : node) {
+				write(child->letter);
+			}
+			for (auto *child : node) {
+				auto [child_offset, child_offset_end]
+					= write(my_offset); // my_offset просто ради std::size_t
+				child_offsets[child] = child_offset;
+			}
+			write(node.is_leaf);
+			if (node.is_leaf) {
+				write(node.value);
+			}
+		}
+		return { offset_start, buffer.size() };
+	}
+};
+class BinaryReader {};
 }
--- include/libtrie/trie.hpp
+++ include/libtrie/trie.hpp
@@ -1,265 +1,250 @@
 #include <cstddef>
 namespace tech {
-    template<class T>
-    class Trie {
-        private:
-        static const std::size_t ALPHABET_SIZE = 26;
-        
-        public:
-        /* member types */
+template <class T> class Trie {
+  private:
+	static const std::size_t ALPHABET_SIZE = 26;
 
-        struct Node {
-            class Iterator {
-            protected:
-                Node **children;
-                std::size_t pos;
-            public:
-                using difference_type = std::ptrdiff_t;
-                using value_type = Node*;
-                using pointer = Node **;
-                using reference = Node* &;
-                using iterator_category = std::forward_iterator_tag;
-                explicit Iterator(Node **ptr, std::size_t cur_pos) : children(ptr), pos(cur_pos) {
-                    while ((pos < ALPHABET_SIZE) && !children[pos++]) {}
-                }
-                Iterator(const Iterator &it) = default;
-                reference operator*() { return children[pos]; }
-                pointer operator->() { return &(children[pos]); }
-                bool operator==(const Iterator &another) const {
-                    return (children == another.children) && (pos == another.pos);
-                }
-                bool operator!=(const Iterator &another) const {
-                    return !((children == another.children) && (pos == another.pos));
-                }
-                Iterator &operator++() {
-                    while ((pos < ALPHABET_SIZE) && !children[pos++]) {}                   
-                    return *this;
-                }
-                Iterator operator++(int) {
-                    auto old = *this;
-                    ++(*this);
-                    return old;
-                }
-            };
-            Iterator begin() {
-                return Iterator(children, 0);
-            }
-            Iterator end() {
-                return Iterator(children, ALPHABET_SIZE);
-            }
-            bool is_leaf = false;
-            T value;
-            char letter;
-            Node* children[ALPHABET_SIZE] = {nullptr};
-            Node* parent = nullptr;
-            // check
-            Node*& operator[](char c) {
-                return children[c - 'a'];
-            }
-            std::size_t has() const {
-                std::size_t count = 0;
-                for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
-                    if (children[i]) {
-                        count++;
-                    }
-                }
-                return count;
-            }
-            bool has(std::size_t count) const {
-                for (auto i = 0; i < ALPHABET_SIZE; ++i) {
-                    if (children[i]) {
-                        count--;
-                        if (count < 0) return false;
-                    }
-                }
-                return count == 0;
-            }
-            Node() = default;
-            Node(Node* ptr, char c) : letter(c), parent(ptr) {}
-            Node(const Node& node) {
-                is_leaf = node.is_leaf;
-                value = node.value;
-                parent = node.parent;
-                letter = node.letter;
-                for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
-                    if (node.children[i]) {
-                        children[i] = new Node(*(node.children[i]));
-                    }
-                }
-            }
-            Node(Node&& node) {
-                is_leaf = node.is_leaf;
-                value = std::move(node.value);
-                parent = node.parent;
-                letter = node.letter;
-                for (auto i = 0; i < ALPHABET_SIZE; ++i) {
-                    if (node.children[i]) {
-                        children[i] = new Node(std::move(*(node.children[i])));
-                    }
-                }
-            }
-            Node(const T& _value) : value(_value), is_leaf(true) {}
-            ~Node() {
-                for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
-                    delete children[i];
-                }
-            }
-        };
-        class Iterator {
-        protected:
-            Node *current;
+  public:
+	/* member types */
 
-        public:
-            using difference_type = std::ptrdiff_t;
-            using value_type = T;
-            using pointer = T *;
-            using reference = T &;
-            using iterator_category = std::forward_iterator_tag;
-            explicit Iterator(Node *ptr) : current(ptr) {}
-            Iterator(const Iterator &it) = default;
-            reference operator*() { return current->value; }
-            pointer operator->() { return &(current->value); }
-            bool operator==(const Iterator &another) const {
-                return current == another.current;
-            }
-            bool operator!=(const Iterator &another) const {
-                return current != another.current;
-            }
-            Iterator &operator++() {
-                if (current) {
-                    current = current->next;
-                }
-                return *this;
-            }
-            Iterator operator++(int) {
-                auto old = *this;
-                ++(*this);
-                return old;
-            }
-        };
+	struct Node {
+		class Iterator {
+		  protected:
+			Node **children;
+			std::size_t pos;
 
-        /* iterators */
-        Iterator begin() {
-            return Iterator(root);
-        }
-        Iterator end() {
-            return Iterator(nullptr);
-        }
+		  public:
+			using difference_type = std::ptrdiff_t;
+			using value_type = Node *;
+			using pointer = Node **;
+			using reference = Node *&;
+			using iterator_category = std::forward_iterator_tag;
+			explicit Iterator(Node **ptr, std::size_t cur_pos)
+				: children(ptr), pos(cur_pos) {
+				while ((pos < ALPHABET_SIZE) && !children[pos++]) {
+				}
+			}
+			Iterator(const Iterator &it) = default;
+			reference operator*() { return children[pos]; }
+			pointer operator->() { return &(children[pos]); }
+			bool operator==(const Iterator &another) const {
+				return (children == another.children) && (pos == another.pos);
+			}
+			bool operator!=(const Iterator &another) const {
+				return !((children == another.children)
+						 && (pos == another.pos));
+			}
+			Iterator &operator++() {
+				while ((pos < ALPHABET_SIZE) && !children[pos++]) {
+				}
+				return *this;
+			}
+			Iterator operator++(int) {
+				auto old = *this;
+				++(*this);
+				return old;
+			}
+		};
+		Iterator begin() { return Iterator(children, 0); }
+		Iterator end() { return Iterator(children, ALPHABET_SIZE); }
+		bool is_leaf = false;
+		T value;
+		char letter;
+		Node *children[ALPHABET_SIZE] = { nullptr };
+		Node *parent = nullptr;
+		// check
+		Node *&operator[](char c) { return children[c - 'a']; }
+		std::size_t has() const {
+			std::size_t count = 0;
+			for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
+				if (children[i]) {
+					count++;
+				}
+			}
+			return count;
+		}
+		bool has(std::size_t count) const {
+			for (auto i = 0; i < ALPHABET_SIZE; ++i) {
+				if (children[i]) {
+					count--;
+					if (count < 0)
+						return false;
+				}
+			}
+			return count == 0;
+		}
+		Node() = default;
+		Node(Node *ptr, char c) : letter(c), parent(ptr) {}
+		Node(const Node &node) {
+			is_leaf = node.is_leaf;
+			value = node.value;
+			parent = node.parent;
+			letter = node.letter;
+			for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
+				if (node.children[i]) {
+					children[i] = new Node(*(node.children[i]));
+				}
+			}
+		}
+		Node(Node &&node) {
+			is_leaf = node.is_leaf;
+			value = std::move(node.value);
+			parent = node.parent;
+			letter = node.letter;
+			for (auto i = 0; i < ALPHABET_SIZE; ++i) {
+				if (node.children[i]) {
+					children[i] = new Node(std::move(*(node.children[i])));
+				}
+			}
+		}
+		Node(const T &_value) : value(_value), is_leaf(true) {}
+		~Node() {
+			for (std::size_t i = 0; i < ALPHABET_SIZE; ++i) {
+				delete children[i];
+			}
+		}
+	};
+	class Iterator {
+	  protected:
+		Node *current;
 
-        /* constructors */
-        Trie() {
-            root = new Node();
-        }
+	  public:
+		using difference_type = std::ptrdiff_t;
+		using value_type = T;
+		using pointer = T *;
+		using reference = T &;
+		using iterator_category = std::forward_iterator_tag;
+		explicit Iterator(Node *ptr) : current(ptr) {}
+		Iterator(const Iterator &it) = default;
+		reference operator*() { return current->value; }
+		pointer operator->() { return &(current->value); }
+		bool operator==(const Iterator &another) const {
+			return current == another.current;
+		}
+		bool operator!=(const Iterator &another) const {
+			return current != another.current;
+		}
+		Iterator &operator++() {
+			if (current) {
+				current = current->next;
+			}
+			return *this;
+		}
+		Iterator operator++(int) {
+			auto old = *this;
+			++(*this);
+			return old;
+		}
+	};
 
-        /* rule of 5 */
-        ~Trie() {
-            clean();
-        }
-        Trie& operator=(const Trie& trie) {
-            clean();
-            root = new Node(*trie.root);
-        }
-        Trie& operator=(Trie&& trie) {
-            clean();
-            root = trie.root;
-        }
-        Trie(const Trie& trie) {
-            root = new Node(*trie.root, 0);
-        }
-        Trie(Trie&& trie) {
-            root = trie.root;
-            trie.root = nullptr;
-        }
-        class Nodes {
-            Node* root;
-            public:
-            class Iterator {
-            protected:
-                Node *current;
+	/* iterators */
+	Iterator begin() { return Iterator(root); }
+	Iterator end() { return Iterator(nullptr); }
 
-            public:
-                using difference_type = std::ptrdiff_t;
-                using value_type = Node;
-                using pointer = Node *;
-                using reference = Node &;
-                using iterator_category = std::forward_iterator_tag;
-                explicit Iterator(Node *ptr) : current(ptr) {}
-                Iterator(const Iterator &it) = default;
-                reference operator*() { return *current; }
-                pointer operator->() { return current; }
-                bool operator==(const Iterator &another) const {
-                    return current == another.current;
-                }
-                bool operator!=(const Iterator &another) const {
-                    return current != another.current;
-                }
-                Iterator &operator++() {
-                    if (!current) {
-                        return *this;
-                    } 
-                    if (current->begin() == current->end()) {
-                        // если у текущего узла нет потомков
-                        // то необходимо вернуться к родителю
-                        // и перейти к потомку который после него
-                        Node* parent = current->parent;
-                        auto it = parent->begin();
-                        while (*it != current) {++it;}
-                        current = *(++it);
-                        // если этого потомка нет (крайний правый)
-                        // то ничего не делаем?
-                    } else {
-                        current = *(++(current->begin()));
-                    }
-                    
-                    return *this;
-                }
-                Iterator operator++(int) {
-                    auto old = *this;
-                    ++(*this);
-                    return old;
-                }
-            };
-            Iterator begin() {
-                return Iterator(root);
-            }
-            Iterator end() {
-                return Iterator(nullptr);
-            }
-            Nodes(Node* node) : root(node) {}
-        };
-        Nodes nodes() const {
-            return Nodes(root);
-        }
+	/* constructors */
+	Trie() { root = new Node(); }
 
-        void add(const std::string& text, T value) {
-            Node* current = root;
-            for (const char& sym : text) {
-                if ((*current)[sym]) {
-                    (*current)[sym] = new Node(current, sym);
-                }
-                current = (*current)[sym];
-            }
-            current->is_leaf = true;
-            current->value = value;
-        }
-        void remove(const std::string& text) {
-            Node* last_branch = nullptr;
-            Node* current = root;
-            for (const auto& sym : text) {
-                if (!(*current)[sym]) {
-                    return;
-                }
-                if (current->has() > 1) {
-                    last_branch = (*current)[sym]; // CHECK
-                }
-                current = (*current)[sym];
-            }
-            delete last_branch;
-        }
-        private:
-            Node* root;
-            void clean() {
-                delete root;
-            }
-    };
+	/* rule of 5 */
+	~Trie() { clean(); }
+	Trie &operator=(const Trie &trie) {
+		clean();
+		root = new Node(*trie.root);
+	}
+	Trie &operator=(Trie &&trie) {
+		clean();
+		root = trie.root;
+	}
+	Trie(const Trie &trie) { root = new Node(*trie.root, 0); }
+	Trie(Trie &&trie) {
+		root = trie.root;
+		trie.root = nullptr;
+	}
+	class Nodes {
+		Node *root;
+
+	  public:
+		class Iterator {
+		  protected:
+			Node *current;
+
+		  public:
+			using difference_type = std::ptrdiff_t;
+			using value_type = Node;
+			using pointer = Node *;
+			using reference = Node &;
+			using iterator_category = std::forward_iterator_tag;
+			explicit Iterator(Node *ptr) : current(ptr) {}
+			Iterator(const Iterator &it) = default;
+			reference operator*() { return *current; }
+			pointer operator->() { return current; }
+			bool operator==(const Iterator &another) const {
+				return current == another.current;
+			}
+			bool operator!=(const Iterator &another) const {
+				return current != another.current;
+			}
+			Iterator &operator++() {
+				if (!current) {
+					return *this;
+				}
+				if (current->begin() == current->end()) {
+					// если у текущего узла нет потомков
+					// то необходимо вернуться к родителю
+					// и перейти к потомку который после него
+					Node *parent = current->parent;
+					auto it = parent->begin();
+					while (*it != current) {
+						++it;
+					}
+					current = *(++it);
+					// если этого потомка нет (крайний правый)
+					// то ничего не делаем?
+				} else {
+					current = *(++(current->begin()));
+				}
+
+				return *this;
+			}
+			Iterator operator++(int) {
+				auto old = *this;
+				++(*this);
+				return old;
+			}
+		};
+		Iterator begin() { return Iterator(root); }
+		Iterator end() { return Iterator(nullptr); }
+		Nodes(Node *node) : root(node) {}
+	};
+	Nodes nodes() const { return Nodes(root); }
+
+	void add(const std::string &text, T value) {
+		Node *current = root;
+		for (const char &sym : text) {
+			if ((*current)[sym]) {
+				(*current)[sym] = new Node(current, sym);
+			}
+			current = (*current)[sym];
+		}
+		current->is_leaf = true;
+		current->value = value;
+	}
+	void remove(const std::string &text) {
+		Node *last_branch = nullptr;
+		Node *current = root;
+		for (const auto &sym : text) {
+			if (!(*current)[sym]) {
+				return;
+			}
+			if (current->has() > 1) {
+				last_branch = (*current)[sym]; // CHECK
+			}
+			current = (*current)[sym];
+		}
+		delete last_branch;
+	}
+
+  private:
+	Node *root;
+	void clean() { delete root; }
+};
 }
